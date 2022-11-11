ead30f1da87d4bf1e43839120f98dd19b0a857bd
Juan Fontenla <juan.fontenla@udc.es>
2022-11-11 17:42:11 +0100

    Added photo grid

--- client/src/modules/book/components/bookListList.vue
+++ client/src/modules/book/components/bookListList.vue
@@ -1,139 +1,122 @@
 <template>
   <v-container v-if="items">
-    <v-card>
-      <v-card-title>
-        <v-row align="center" justify="space-between" no-gutters>
-          <v-col class="d-none d-md-block">
-            <span class="headline no-split-words">
-              {{ $t($route.meta.label) }}
-            </span>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="7"
-            md="4"
-            lg="6"
-            xl="8"
-            order="2"
-            order-sm="1"
-            class="text-center text-sm-right mt-4 mt-sm-0"
-          >
-            <v-btn color="primary" outlined @click="showFilters = !showFilters">
-              <v-icon left dark>mdi-magnify</v-icon>
-              <span>
-                {{ showFilters ? $t("hideFilters") : $t("showFilters") }}
-              </span>
-              <v-icon right dark v-if="showFilters">mdi-chevron-up</v-icon>
-              <v-icon right dark v-else>mdi-chevron-down</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col order="1" order-md="2" class="text-right">
-            <v-btn color="primary" @click="reportRequest()">
-              <v-icon>import_export</v-icon>
-              <span class="d-none d-sm-block"> {{ $t("export") }} </span>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-title>
-
-      <v-card-text>
-        <v-row align="center" v-show="showFilters" justify="space-between">
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="isbnFilter"
-              :label="$t('t_book.prop.isbn')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="titleFilter"
-              :label="$t('t_book.prop.title')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="categoryFilter"
-              :label="$t('t_book.prop.category')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="editorialFilter"
-              :label="$t('t_book.prop.editorial')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="photoFilter"
-              :label="$t('t_book.prop.photo')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <debounced-text-field
-              dense
-              v-model="authorFilter"
-              :label="$t('t_book.prop.author')"
-            ></debounced-text-field>
-          </v-col>
-          <v-col cols="12" md="2" xl="1">
-            <v-select
-              dense
-              @change="redirectOnFilterChange"
-              clearable
-              :items="sourceProperty"
-              :item-text="(item) => $t(item.text)"
-              item-value="value"
-              :menu-props="{ offsetY: true }"
-              v-model="sourceFilter"
-              :label="$t('t_book.prop.source')"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="6" md="2" xl="1">
-            <number-field
-              :debouncing="300"
-              v-model="priceFilter"
-              type="double"
-              :label="$t('t_book.prop.price')"
-            ></number-field>
-          </v-col>
-        </v-row>
-        <v-data-table
-          :headers="headers"
-          :items="items"
-          :options="entitiesPage"
-          :server-items-length="totalItems"
-          :loading="loading"
-          :footer-props="tableFooterProps"
-          @click:row="entityDetail"
-          @update:options="redirectOnTableChange"
+    <v-row align="center" justify="space-between" class="mt-4">
+      <v-col cols="9" class="text-center">
+        <debounced-text-field
+          dense
+          v-model="search"
+          :label="$t('search')"
+        ></debounced-text-field>
+      </v-col>
+      <v-col cols="3" class="text-right">
+        <v-btn color="primary" outlined @click="showFilters = !showFilters">
+          <v-icon left dark>mdi-magnify</v-icon>
+          <span>
+            {{ showFilters ? $t("hideFilters") : $t("showFilters") }}
+          </span>
+          <v-icon right dark v-if="showFilters">mdi-chevron-up</v-icon>
+          <v-icon right dark v-else>mdi-chevron-down</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row align="center" v-show="showFilters" justify="space-between">
+      <v-col cols="12" md="4">
+        <debounced-text-field
+          dense
+          hide-details
+          v-model="categoryFilter"
+          :label="$t('t_book.prop.category')"
+        ></debounced-text-field>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-select
+          dense
+          @change="redirectOnFilterChange"
+          clearable
+          hide-details
+          :items="sourceProperty"
+          :item-text="(item) => $t(item.text)"
+          item-value="value"
+          :menu-props="{ offsetY: true }"
+          v-model="sourceFilter"
+          :label="$t('t_book.prop.source')"
         >
-          <template v-slot:[`item.source`]="{ item }">
-            <span v-if="item.source">
-              {{ $t(`source.${item.source}`) }}
-            </span>
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-range-slider
+          v-model="priceFilter"
+          max="1000"
+          hide-details
+          class="align-center"
+          :label="$t('t_book.prop.price')"
+        >
+          <template v-slot:prepend>
+            <v-text-field
+              :value="priceFilter[0]"
+              class="mt-0 pt-0"
+              hide-details
+              single-line
+              type="number"
+              style="width: 60px"
+              @change="$set(priceFilter, 0, $event)"
+            ></v-text-field>
           </template>
-
-          <template v-slot:[`item.action`]="{ item }">
-            <v-icon color="primary" @click.stop="entityDetail(item)">
-              description
-            </v-icon>
+          <template v-slot:append>
+            <v-text-field
+              :value="priceFilter[1]"
+              class="mt-0 pt-0"
+              hide-details
+              single-line
+              type="number"
+              style="width: 60px"
+              @change="$set(priceFilter, 1, $event)"
+            ></v-text-field>
           </template>
-        </v-data-table>
-      </v-card-text>
-    </v-card>
+        </v-range-slider>
+      </v-col>
+    </v-row>
+    <v-row v-if="mockData && mockData.length > 0">
+      <v-col
+        class="mb-12"
+        v-for="book in mockData"
+        cols="12"
+        md="4"
+        lg="3"
+        :key="book.isbn"
+      >
+        <v-hover v-slot="{ hover }">
+          <v-card
+            :elevation="hover ? 16 : 0"
+            flat
+            class="{ 'on-hover': hover }"
+          >
+            <v-row>
+              <v-col class="text-center">
+                <v-img height="200" contain :src="book.photo"></v-img>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="text-center overline">
+                {{ parseBookTitle(book.title) }}
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col class="text-center h4">
+        <span v-if="anySearch">{{ $t("no-data.no-results") }}</span>
+        <span v-else>{{ $t("no-data.placeholder") }}</span>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import DebouncedTextField from "@/components/debouncing-inputs/DebouncedTextField.vue";
 
-import NumberField from "@/components/number-field/NumberField.vue";
 import response from "@/common/mock-response";
 import defaultPaginationSettings from "@/common/default-pagination-settings";
 import {
@@ -149,16 +132,21 @@ import reportRequestFn from "@/common/ReportRequest";
 import RepositoryFactory from "@/repositories/RepositoryFactory";
 const BookEntityRepository = RepositoryFactory.get("BookEntityRepository");
 
+const lowerCaseAllWordsExceptFirstLetters = (string) =>
+  string.replaceAll(
+    /\S*/g,
+    (word) => `${word.slice(0, 1)}${word.slice(1).toLowerCase()}`
+  );
+
 export default {
   name: "bookListList",
   components: {
     DebouncedTextField,
-    NumberField,
   },
   data() {
     return {
-      mockData: response,
       items: [],
+      search: null,
       showFilters: false,
       sourceProperty: sourceProperty,
       isbnFilter: null,
@@ -168,7 +156,7 @@ export default {
       photoFilter: null,
       authorFilter: null,
       sourceFilter: null,
-      priceFilter: null,
+      priceFilter: [0, 1000],
       entitiesPage: {
         page:
           parseInt(this.$route.query.page) || defaultPaginationSettings.page,
@@ -184,6 +172,12 @@ export default {
     };
   },
   computed: {
+    anySearch() {
+      return this.categoryFilter || this.sourceFilter || this.priceFilter;
+    },
+    mockData() {
+      return response.hits.hits.map((el) => el._source);
+    },
     headers() {
       return [
         {
@@ -308,6 +302,9 @@ export default {
     this.getItems();
   },
   methods: {
+    parseBookTitle(title) {
+      return lowerCaseAllWordsExceptFirstLetters(title);
+    },
     getItems() {
       this.loading = true;
       const options = {
